ecee1273ab8e384911a1575a2f00d6df2521ed01
Zhu Li <zhuli19901106@gmail.com>
2014-03-31 20:12:09 +0800

    add careercup files - google
    201403312011

---
+++ google-5424071030341632.cpp
@@ -0,0 +1,8 @@
+// http://www.careercup.com/question?id=5424071030341632
+// Answer:
+//	A trie can allow you to find the longest common prefix qickly, at the cost of huge space.
+//	By reversing the string, you could find out the longest common postfix in a similar manner.
+int main()
+{
+	return 0;
+}
