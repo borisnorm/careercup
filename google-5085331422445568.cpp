d752471885dd0acee550cd73af31edd304382775
Zhu Li <zhuli19901106@gmail.com>
2014-03-31 20:40:09 +0800

    add careercup files - google
    201403312040

---
+++ google-5085331422445568.cpp
@@ -0,0 +1,21 @@
+// http://www.careercup.com/question?id=5085331422445568
+// Answer:
+//	Dijkstra Algorithm is not for TSP, why would you expect to solve NP-hard problems with O(n ^ 2) efforts?
+// Comment:
+//	1. Difficult question.
+//	2. Vague description.
+//	3. Strange indication.
+//	4. Stupid discussion.
+// Based on the judgement above, I believe that 'guy' is a complete amateur. Excellent programmers don't ask stupid questions, but he did.
+// The reason I'm complaining is that, he pretended to have google interview experience and posted a lot of fake questions only to trick us.
+// He doesn't even know how to calculate the distance between two points?
+// He doesn't know what volatile means.
+// He keeps asking stupid questions.
+// SO HOW THE HELL DOES THIS 'GUY' HAVE SO MANY GOOGLE INTERVIEWS?
+// He's site reliability engineer at Boeing, really?
+// I hate liars, especially foolish liars!
+// Down vote for every question he posted.
+int main()
+{
+	return 0;
+}
