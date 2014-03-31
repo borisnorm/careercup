469643f4b886c8236d8601c890ba7a43a195b0fd
Zhu Li <zhuli19901106@gmail.com>
2014-03-31 19:52:41 +0800

    add careercup files - google
    201403311952

---
+++ google-6331648220069888.cpp
@@ -0,0 +1,9 @@
+// http://www.careercup.com/question?id=5735304249999360
+// Answer:
+//	What is the meaning of tracking algorithm of nearest location to some friends?
+//	Finding a point such that the sum of all friends' distances to it is minimized?
+//	That 'guy' again...
+int main()
+{
+	return 0;
+}
