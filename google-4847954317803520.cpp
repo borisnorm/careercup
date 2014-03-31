16232ace5a39352aad403cc46435ce92c30df579
Zhu Li <zhuli19901106@gmail.com>
2014-03-31 17:10:41 +0800

    add careercup files - google
    201403311710

---
+++ google-4847954317803520.cpp
@@ -0,0 +1,11 @@
+// http://www.careercup.com/question?id=4847954317803520
+// For n bits, signed integer can reach 2 ^ (n - 1) - 1.
+// For n bits, unsigned integer can reach 2 ^ n - 1.
+// My question is: is this a real Google interview question? Onsite interview?
+// This guy named 'guy' has been posting an awful lot of questions on Careercup, which contains some difficult, tricky, trivial and stupid ones.
+// I doubt if he's simply trying to gather more reputation, by means of spam questions. If he's not telling the truth, he's polluting Careercup and misleading other visitors here.
+// He should be warned and punished.
+int main()
+{
+	return 0;
+}
---
+++ google-5765091433644032.cpp
@@ -0,0 +1,17 @@
+// http://www.careercup.com/question?id=5765091433644032
+// Transfer a binary tree to another machine? Of course, serialization and deserialization.
+// Any kind of tree traversal with the help of a special notation to represent 'NULL' can do the serialization.
+// For example, preorder traversal for the tree below:
+//               1
+//             /   \
+//            2     8
+//           / \   / \
+//          7  12 34  9
+//
+// If we use '#' to represent NULL, the preorder traversal looks like {1, 2, 7, #, #, 12, #, #, 8, 34, #, #, 9, #, #}
+// With this you can output the array to a text file.
+// The '#' notation ensures that every binary tree has its unique preorder traversal.
+int main()
+{
+	return 0;
+}
