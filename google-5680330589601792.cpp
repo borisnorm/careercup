2169a521fb584b8188443ea8100e922cefd19c31
Zhu Li <zhuli19901106@gmail.com>
2014-03-31 20:21:11 +0800

    add careercup files - google
    201403312021

---
+++ google-5680330589601792.cpp
@@ -0,0 +1,8 @@
+// http://www.careercup.com/question?id=5680330589601792
+// Answer:
+//	If the server is being flooded by huge succession of queries, a must-do is caching. Maybe LRU, maybe automatically adjusted by other conditions.
+//	The caching is based on some kind of hashing, with the digital sign of the query as the hash key. The query result is cached inside.
+int main()
+{
+	return 0;
+}
